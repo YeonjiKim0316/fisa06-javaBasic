1a5b86b3cbd5faf95a1355aa066cd06f08686009
YeonjiKim0316 <kimyeonji3@gmail.com>
2026-04-27 11:59:00 +0900

    LapTop interface

--- untitled/src/computerInterface/Laptop.java
+++ untitled/src/computerInterface/Laptop.java
@@ -1,4 +1,15 @@
 package computerInterface;
 
-public class Laptop {
+// interface를 상속받는 것을 extends 가 아닌
+// "구현" implements 라는 키워드로 연결해서 사용
+public class Laptop implements Computer {
+    @Override
+    public void powerOn() {
+
+    }
+
+    @Override
+    public void powerOff() {
+
+    }
 }
